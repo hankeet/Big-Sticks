cd54948aeac4188ade183842df1c8feb16b14a8c
wxh <289110789@qq.com>
2017-10-17 15:06:09 +0800

    1234

---
+++ Y/App-Student/Hui/Controllers/HuiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hui.Controllers
+{
+    public class HuiController : Controller
+    {
+        // GET: Hui
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
